b82a9dd7a57f2b326200b6fa1697dfd84a63a4e0
CierraSimms <cierrasimms98@gmail.com>
2022-03-09 16:17:58 -0500

    complete

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,21 +7,21 @@ end
 
 def get_first_product
   first_product = Product.first
-  render json: first_product.as_json
+  render json: {product1: first_product}
 end
 
 def get_second_product
   second_product = Product.second
-  render json: second_product.as_json
+  render json: {product2: second_product}
 end
 
 def get_third_product
   third_product = Product.third
-  render json: third_product.as_json
+  render json: {product3: third_product}
 end
 
 def get_fourth_product
   fourth_product = Product.fourth
-  render json: fourth_product.as_json
+  render json: {product4: fourth_product}
 end
 end
--- products_http.rb
+++ products_http.rb
@@ -11,16 +11,16 @@ input_option = gets.chomp
 
 if input_option.to_i == 1
   response = HTTP.get("http://localhost:3000/first_product")
-  puts response.parse["first_product"]
+  puts response.parse["product1"]
 elsif input_option.to_i == 2
   response = HTTP.get("http://localhost:3000/second_product")
-  puts response.parse["second_product"]
+  puts response.parse["product2"]
 elsif input_option.to_i == 3
   response = HTTP.get("http://localhost:3000/third_product")
-  puts response.parse["third_product"]
+  puts response.parse["product3"]
 elsif input_option.to_i == 4
   response = HTTP.get("http://localhost:3000/fourth_product")
-  puts response.parse["fourth_product"]
+  puts response.parse["product4"]
 else
   puts input_option
 end
