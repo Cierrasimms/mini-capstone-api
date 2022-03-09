0b196d3460075d1082dd6c8a4b6297db719105e5
CierraSimms <cierrasimms98@gmail.com>
2022-03-08 20:48:08 -0500

    added products in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,7 @@
-P
+Product.new(name: "Lifelike Inflatable Elephant", price: 60, image_url: "n/a", description: "19 inch grey inflatable elephant")
+
+Product.new(name: "Meat Shredder Claws", price: 25, image_url: "n/a", description: "Wolverine claws to shred meat with")
+
+Product.new(name: "Nicolas Cage Pillow", price: 40, image_url: "https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1632326476-51qsxODMtYL._SL500_.jpg?crop=1xw:1.00xh;center,top&resize=768:*", description: "Pillow with Nicolas Cage printed on it")
+
+Product.new(name: "Thanos One-Piece Swimsuit for Men", price: 78, image_url: "n/a", description: "Men's swimsuit with an image of Thanos printed on it")
