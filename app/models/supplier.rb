6d4a8933cac12ca6bb08f03af31a06d377a18ec9
CierraSimms <cierrasimms98@gmail.com>
2022-03-20 10:35:41 -0400

    Added Image model with url and product_id

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -2,7 +2,10 @@ class Supplier < ApplicationRecord
   validates :name, presence: true
   validates :name, uniqueness: true
   validates :email, presence: true
+  validates :email, uniqueness: true
+  validates :phone_number, uniqueness: true
   validates :phone_number, presence: true
+  
 
 has_many :products
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,3 @@
-Product.create!([
-  {name: "Meat Shredder Claws", price: "25.0", image_url: "n/a", description: "This surprisingly efficient way of shredding your meat has the added in bonus of making you feel like Wolverine.", item_count: 89},
-  {name: "KFC Romance Novel", price: "40.0", image_url: "https://cdn.thisiswhyimbroke.com/buying-guides-thumb/54/kfc-romance-novel_400x.jpg", description: "The enchanting tale on the pages of this KFC romance novel is so great it's, dare we say it, finger-lickin' good. Read how the colonel comes to the rescue of a damsel in distress complete with cheesy dialog.", item_count: 1},
-  {name: "The Hen Bag Handbag", price: "25.0", image_url: "https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1571683417-rubber-chicken-purse-1571683378.jpg?crop=1.00xw:0.667xh;0,0.168xh&resize=768:*", description: "Is that the latest Hermès bag straight off the runway? No, it's a rubber chicken purse from Amazon.", item_count: 56},
-  {name: "Nicolas Cage Pillow", price: "5.0", image_url: "https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1571689550-51Q0xuEOjRL.jpg?crop=1xw:1.00xh;center,top&resize=768:*", description: "No words for this one.", item_count: 7},
-  {name: "Lifelike Inflatable Elephant", price: "60.0", image_url: "https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1571692002-inflatable-elephant-1571691977.jpg?crop=1.00xw:0.667xh;0,0.220xh&resize=768:*", description: "Try to toss a hula hoop over the trunk of the 19-inch tall inflatable elephant, because why not?", item_count: 10},
-  {name: "Realistic Dog Head Masks", price: "100.0", image_url: "https://cdn.thisiswhyimbroke.com/buying-guides-thumb/54/realistic-dog-head-masks_400x.jpg", description: "Take a trip to the dog park wearing this mask and NO ONE will think you've gone off the deep end. The high-quality mask is made from 100% latex and fits most adult heads.", item_count: 22},
-  {name: "Thanos One-Piece Swimsuit for Men", price: "78.0", image_url: "https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1565295660-412g9mL-i8L.jpg?crop=1xw:1.00xh;center,top&resize=768:*", description: "Dressing up as the ultimate villain will give you all the confidence come beach day.", item_count: 12},
-  {name: "Stuffed Blobfish Plush", price: "20.0", image_url: "https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1632326997-31v8fPnS8UL._SL500_.jpg?crop=1xw:1.00xh;center,top&resize=768:*", description: "In 2013, the gelatinous blobfish won the 'World's Ugliest Animal' award. But somehow, these mini plush dolls make them seem kind of adorable.", item_count: 67},
-  {name: "Mac Inspired Candle", price: "30.0", image_url: "https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1571691785-31gZkTuUknL.jpg?crop=1xw:1.00xh;center,top&resize=768:*", description: "You may not be able to afford a new Macbook (even though Apple has some great Black Friday deals coming up), but you'll be able to get that new Mac smell with this inspired candle", item_count: 47}
-])
-
-
 Supplier.create!([
   {name: "Amazon", email: "amazon@test.com", phone_number: "(800)987-6543"},
   {name: "Walmart", email: "walmart@test.com", phone_number: "(800)756-3455"},
@@ -18,3 +5,20 @@ Supplier.create!([
 
 
 ])
+
+
+
+Product.create!([
+  {name: "Meat Shredder Claws", price: "25.0", image_url: "https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1571689550-51Q0xuEOjRL.jpg?crop=1xw:1.00xh;center,top&resize=768:*", description: "This surprisingly efficient way of shredding your meat has the added in bonus of making you feel like Wolverine.", item_count: 89, supplier_id: 1},
+  {name: "KFC Romance Novel", price: "40.0", image_url: "https://cdn.thisiswhyimbroke.com/buying-guides-thumb/54/kfc-romance-novel_400x.jpg", description: "The enchanting tale on the pages of this KFC romance novel is so great it's, dare we say it, finger-lickin' good. Read how the colonel comes to the rescue of a damsel in distress complete with cheesy dialog.", item_count: 1,supplier_id: 1},
+  {name: "The Hen Bag Handbag", price: "25.0", image_url: "https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1571683417-rubber-chicken-purse-1571683378.jpg?crop=1.00xw:0.667xh;0,0.168xh&resize=768:*", description: "Is that the latest Hermès bag straight off the runway? No, it's a rubber chicken purse from Amazon.", item_count: 56,supplier_id: 1},
+  {name: "Nicolas Cage Pillow", price: "5.0", image_url: "https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1571689550-51Q0xuEOjRL.jpg?crop=1xw:1.00xh;center,top&resize=768:*", description: "No words for this one.", item_count: 7, supplier_id: 2},
+  {name: "Lifelike Inflatable Elephant", price: "60.0", image_url: "https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1571692002-inflatable-elephant-1571691977.jpg?crop=1.00xw:0.667xh;0,0.220xh&resize=768:*", description: "Try to toss a hula hoop over the trunk of the 19-inch tall inflatable elephant, because why not?", item_count: 10,supplier_id: 2},
+  {name: "Realistic Dog Head Masks", price: "100.0", image_url: "https://cdn.thisiswhyimbroke.com/buying-guides-thumb/54/realistic-dog-head-masks_400x.jpg", description: "Take a trip to the dog park wearing this mask and NO ONE will think you've gone off the deep end. The high-quality mask is made from 100% latex and fits most adult heads.", item_count: 22,supplier_id: 2},
+  {name: "Thanos One-Piece Swimsuit for Men", price: "78.0", image_url: "https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1565295660-412g9mL-i8L.jpg?crop=1xw:1.00xh;center,top&resize=768:*", description: "Dressing up as the ultimate villain will give you all the confidence come beach day.", item_count: 12, supplier_id: 3},
+  {name: "Stuffed Blobfish Plush", price: "20.0", image_url: "https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1632326997-31v8fPnS8UL._SL500_.jpg?crop=1xw:1.00xh;center,top&resize=768:*", description: "In 2013, the gelatinous blobfish won the 'World's Ugliest Animal' award. But somehow, these mini plush dolls make them seem kind of adorable.", item_count: 67, supplier_id: 3},
+  {name: "Mac Inspired Candle", price: "30.0", image_url: "https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1571691785-31gZkTuUknL.jpg?crop=1xw:1.00xh;center,top&resize=768:*", description: "You may not be able to afford a new Macbook (even though Apple has some great Black Friday deals coming up), but you'll be able to get that new Mac smell with this inspired candle", item_count: 47, supplier_id: 3}
+])
+
+
+
