757a0f3466eb7f9f45c2e63c9945beccce7fa43c
CierraSimms <cierrasimms98@gmail.com>
2022-04-20 20:08:37 -0400

    updates

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,47 +1,46 @@
 class ImagesController < ApplicationController
-
-def index
-  all_images = Image.all
-  render json: all_images
-end
-
-def show
-  image = Image.find_by(id: params["image_id"])
-  render json: image
-end
-
-def create
-  image = Image.new(
-    
-    url: params["url"]
-  )
-  if product.save
-    render json: image
-    else
-      render json: {error_massages: image.errors.full_messages}, status: 422
-    end
+  def index
+    all_images = Image.all
+    render json: all_images
   end
-
-  def update
-    image_id = params["id"]
-    image = Image.find(image)
   
-    image.url = params["url"] || image.url
+  def show
+    image = Image.find_by(id: params["image_id"])
+    render json: image
+  end
   
-  
-    if image.save
+  def create
+    image = Image.new(
+      
+      url: params["url"]
+    )
+    if product.save
       render json: image
       else
-        render json: {error_massages: image.errors.full_messages},status: 422
+        render json: {error_massages: image.errors.full_messages}, status: 422
+      end
     end
-  end
   
-  def destroy
-    image_id = params["id"]
-    image = Image.find_by(id: image_id)
-    image.destroy
-    render json: "Image has been terminated."
+    def update
+      image_id = params["id"]
+      image = Image.find(image)
+    
+      image.url = params["url"] || image.url
+    
+    
+      if image.save
+        render json: image
+        else
+          render json: {error_massages: image.errors.full_messages},status: 422
+      end
+    end
+    
+    def destroy
+      image_id = params["id"]
+      image = Image.find_by(id: image_id)
+      image.destroy
+      render json: "Image has been terminated."
+    end
+    
   end
-  
-end
 end
